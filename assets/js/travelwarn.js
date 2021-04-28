70d5ae621c038f4fe5961de75312854708b9d900
Mengqiao-Cai <mengqiao@bu.edu>
2021-04-28 18:06:07 -0400

    added js

---
+++ assets/js/travelwarn.js
@@ -0,0 +1,6 @@
+var ans = prompt("Would you like to learn about travel in Boston? If yes, put 'yes'. If no, put 'no'.");
+if (ans == "yes") {
+	alert("You answered " + ans + ". Enjoy it!");}
+else if (ans == "no"){
+	alert("You answered " + ans + ". It seems you are familar with Boston! Please let us know too!");
+}
